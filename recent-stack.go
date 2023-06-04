688940f6038b8520015533cad8dd106fe975487f
michael gerber <michael.gerber@betvictor.com>
2023-06-04 20:25:28 +0200

    init

---
+++ recent-stack.go
@@ -0,0 +1,15 @@
+package recentstack
+
+import (
+	"errors"
+
+	deque "gopkg.in/dnaeon/go-deque.v1"
+)
+
+func New() error {
+	var qq *deque.Deque[int]
+	if qq == nil {
+		return errors.New("not implemented")
+	}
+	return errors.New("not implemented")
+}
